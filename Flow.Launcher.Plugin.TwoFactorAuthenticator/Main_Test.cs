e445cb8dd242a6a34a97d3e9ca46302858fa9be7
YuXiaoyao <zxyfuncy@gmail.com>
2024-02-18 22:20:58 +0800

    prepare add google otp-migration

--- Flow.Launcher.Plugin.TwoFactorAuthenticator/Main_Test.cs
+++ Flow.Launcher.Plugin.TwoFactorAuthenticator/Main_Test.cs
@@ -1,9 +1,25 @@
-﻿namespace Flow.Launcher.Plugin.TwoFactorAuthenticator;
+﻿using Flow.Launcher.Plugin.TwoFactorAuthenticator.Migration;
+
+namespace Flow.Launcher.Plugin.TwoFactorAuthenticator;
 
 public class Main_Test
 {
     // PS private info 
     public static void Main()
     {
+        resolveUrl();
+    }
+
+    public static void resolveUrl()
+    {
+        var url =
+            "otpauth-migration://offline?data=";
+
+        var m1 = OtpMigrationUtil.ParseOtpMigration(url);
+
+        var url2 =
+            "otpauth-migration://offline?data=";
+
+        var m2 = OtpMigrationUtil.ParseOtpMigration(url2);
     }
 }
--- Readme.md
+++ Readme.md
@@ -6,3 +6,15 @@ A plugin for the [Flow launcher](https://github.com/Flow-Launcher/Flow.Launcher)
 ### Usage
 
     * <arguments>
+
+
+
+
+
+## Google Protobuf
+
+> custom Generate Migration.cs file
+
+```shell
+protoc --csharp_out=./Migration Migration/migration.proto
+```

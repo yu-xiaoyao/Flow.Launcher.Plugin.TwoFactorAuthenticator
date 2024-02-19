638ba342a1c992ae5dcd052391846af15878ab63
YuXiaoyao <zxyfuncy@gmail.com>
2024-02-19 16:22:03 +0800

    add otp migration

---
+++ Flow.Launcher.Plugin.TwoFactorAuthenticator/Main_Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Flow.Launcher.Plugin.TwoFactorAuthenticator.Migration;
+
+namespace Flow.Launcher.Plugin.TwoFactorAuthenticator;
+
+public class Main_Test
+{
+    // PS private info 
+    public static void Main()
+    {
+        resolveUrl1();
+        resolveUrl2();
+    }
+
+
+    public static void resolveUrl1()
+    {
+        var url =
+            "otpauth-migration://offline?data=";
+
+        var m1List = OtpMigrationUtil.ParseOtpMigration(url);
+
+        if (m1List != null)
+        {
+            foreach (var otpParam in m1List)
+            {
+                Console.WriteLine(otpParam);
+            }
+
+            var test = m1List[0];
+            Console.WriteLine($"test = {test}");
+            var code = TotpUtil.GenerateTOTPPinCode(test);
+            Console.WriteLine($"code = {code}");
+        }
+    }
+
+    public static void resolveUrl2()
+    {
+        var url =
+            "otpauth-migration://offline?data=";
+
+        var m1List = OtpMigrationUtil.ParseOtpMigration(url);
+
+        if (m1List != null)
+        {
+            foreach (var otpParam in m1List)
+            {
+                Console.WriteLine(otpParam);
+            }
+
+            var test = m1List[0];
+            Console.WriteLine($"test = {test}");
+            var code = TotpUtil.GenerateTOTPPinCode(test);
+            Console.WriteLine($"code = {code}");
+        }
+    }
+}
